0bdf17b43180afd521ed97f4efb6d0d53eff35c1
AnatoliiYakubyshyn <ayakubyshyn@solvd.com>
2024-05-01 10:47:21 +0300

    inited template

---
+++ Drivers/Driver.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Specflow.Drivers
+{
+    public class Driver
+    {
+
+    }
+}
---
+++ Hooks/Hook.cs
@@ -0,0 +1,11 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace Specflow.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        
+    }
+}
