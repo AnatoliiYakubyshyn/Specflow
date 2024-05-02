731c90e552aa7e0131650d3b078d90d4e47be4b0
AnatoliiYakubyshyn <ayakubyshyn@solvd.com>
2024-05-02 10:18:36 +0300

    c1

---
+++ Steps/Shop.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TechTalk.SpecFlow;
+
+namespace Specflow.Steps
+{
+    [Binding]
+    public class Shop
+    {
+        
+    }
+}
